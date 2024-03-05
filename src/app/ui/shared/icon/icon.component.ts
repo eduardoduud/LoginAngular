b131f6efc5dc5d4c44a77e5f8848fa566f4b25e7
Eduardo <eeadomingues@gmail.com>
2024-03-04 21:43:01 -0300

    identation and styles adjustments

--- src/app/ui/shared/icon/icon.component.ts
+++ src/app/ui/shared/icon/icon.component.ts
@@ -2,23 +2,21 @@ import {ChangeDetectionStrategy, Component, Input} from "@angular/core";
 import {IconModule} from "./icon.module";
 import {SvgIconComponent} from "angular-svg-icon";
 
-// @ts-ignore
 
 @Component({
   standalone: true,
   selector: 'app-icon',
-  template: `<svg-icon
-                       [src]="iconPath"
-                       [svgClass]="block"
+  template: `<svg-icon [src]="iconPath"
+                       [svgClass]="svgClass"
                        [applyClass]="true">
-             </svg-icon>`,
+  </svg-icon>`,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [IconModule, SvgIconComponent]
 })
 
 export class IconComponent {
   iconPath = '';
-  @Input() block?: string;
+  @Input() svgClass?: string;
   @Input() set type(type:string) {
     this.iconPath = `assets/images/${type}.svg`
   };
